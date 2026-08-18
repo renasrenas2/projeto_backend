127402a028f02d296dd1ac3ad367400a493f71e4
renasrenas2 <renato.moreira.faria@gmail.com>
2026-08-18 14:56:03 -0300

    Adicionando o programa API_SQL.py

---
+++ API_SQL.py
@@ -0,0 +1,20 @@
+from flask import Flask, jsonify, request
+import sqlite3
+
+app = Flask(__name__)
+
+def conectar_banco():
+    conexao = sqlite3.connect("biblioteca.db")
+    conexao.row_factory = sqlite3.Row
+    return conexao
+
+def criar_tabela():
+    conexao = conectar_banco()
+
+    conexao.execute(
+        CREATE TABLE IF NOT EXISTS livros(
+            id INTEGER PRIMARY KEY AUTOINCREMENT,
+            titulo TEXT NOT NULL,
+            autor TEXT NOT NULL,
+        )   
+    )
